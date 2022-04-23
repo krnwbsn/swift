9b92e3f8f48fbfd6ccd185e132c2cae9ff6b1e7c
Krina Wibisana <krinawibisana@gmail.com>
2022-04-23 13:00:59 +0700

    [loops] learning loops in swift using whale-talk task

---
+++ whale-talk.swift
@@ -0,0 +1,19 @@
+var input: String = "whale talk"
+var output: String = ""
+
+for char in input {
+  let lowerChar = char.lowercased()
+
+  switch lowerChar {
+    case "a", "i", "o":
+      output += lowerChar.uppercased()
+    case "e":
+      output += "EE"
+    case "u":
+      output += "UU"
+    default:
+    continue
+  }
+}
+
+print(output)

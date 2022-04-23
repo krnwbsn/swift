a2a170740e03e88a764b80978ca185445856dbd6
Krina Wibisana <krinawibisana@gmail.com>
2022-04-23 12:58:06 +0700

    [variables] learning basic math and string interpolation

---
+++ piggy-bank.swift
@@ -0,0 +1,8 @@
+var pesos: Double = 10.54 // 0,050
+var reais: Double = 8.12 // 0,22
+var soles: Double = 9.6 // 0,27
+
+var total: Double = 0
+
+total = (0.050 * pesos) + (0.22 * reais) + (0.27 * soles)
+print("US Dollars = $\(total)")

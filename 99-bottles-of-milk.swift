735141363511041a0b1dadb26cc51e38109dbcf4
Krina Wibisana <krinawibisana@gmail.com>
2022-04-23 13:02:48 +0700

    [loops] learning loops in swift using 99 bottles of milk task

---
+++ 99-bottles-of-milk.swift
@@ -0,0 +1,12 @@
+var numBottles: Int = 99
+
+while numBottles > 0 {
+  print("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!")
+  print("You take one down and pass it around…")
+
+  numBottles -= 1
+
+  print("\(numBottles) bottles of milk on the wall!")
+}
+
+print("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!\nYou take one down and pass it around…\n\(numBottles) bottles of milk on the wall!")

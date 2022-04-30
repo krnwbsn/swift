0cb79140ac9e48bcfb679ee50021141ce3999041
Krina Wibisana <krinawibisana@gmail.com>
2022-04-30 22:52:31 +0700

    [escaping] learning escaping in swift

---
+++ escaping.swift
@@ -0,0 +1,23 @@
+struct NumberPrinter {
+    var startAction: () -> Void
+    var completeAction: () -> Void
+    
+    mutating func setStartAction(to action: @escaping () -> Void) {
+        self.startAction = action
+    }
+    
+    mutating func setCompleteAction(to action: @escaping () -> Void) {
+        self.completeAction = action
+    }
+    
+    func printNumber(using numberGenerator: () -> Int) {
+        startAction()
+        print(numberGenerator())
+        completeAction()
+    }
+}
+
+let printer = NumberPrinter(startAction: { print("Starting!") }, completeAction: { print("Finished!") })
+
+printer.printNumber(using: { Int.random(in: 1..<100) })
+printer.printNumber(using: { Int.random(in: 1..<5) })
